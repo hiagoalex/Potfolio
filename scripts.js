2a737f1c800346b41a786a0dc4e02667865275c4
Hiago Alex Vitorino Bertoldo <103057393+hiagoalex@users.noreply.github.com>
2024-08-14 12:46:04 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    ScrollReveal().reveal('#home, #about, #projects, #contact', {
+        delay: 200,
+        distance: '50px',
+        duration: 1000,
+        easing: 'ease-in-out',
+        origin: 'bottom',
+        reset: true
+    });
+});
